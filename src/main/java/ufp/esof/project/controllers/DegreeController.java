a6e9501bb7d7eefcc86cd69ec371c3ea34715396
João Castro <joaocastro@LAPTOP-O7Q8QRDC.localdomain>
2019-12-04 18:26:11 +0000

    Post request implementation

--- src/main/java/ufp/esof/project/controllers/DegreeController.java
+++ src/main/java/ufp/esof/project/controllers/DegreeController.java
@@ -1,13 +1,11 @@
 package ufp.esof.project.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import ufp.esof.project.models.Degree;
 import ufp.esof.project.repositories.DegreeRepo;
 
@@ -25,8 +23,19 @@ public class DegreeController {
 
     @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Degree> createDegree(@RequestBody Degree degree) {
-        // insert into database (h2)
-        System.out.println(degree.toString());
+        //TODO: move logic to service
+        if (this.degreeRepo.findByName(degree.getName()).isPresent()) {
+            throw new DegreeAlreadyExistsException(degree.getName());
+        }
+
         return ResponseEntity.ok().build();
+    }
+
+    //TODO: move logic to service
+    @ResponseStatus(value = HttpStatus.BAD_REQUEST, reason = "Client already exists")
+    public static class DegreeAlreadyExistsException extends RuntimeException {
+        public DegreeAlreadyExistsException(String name) {
+            super("A degree with name \"" + name + "\" already exists");
+        }
     }
 }
--- src/main/java/ufp/esof/project/repositories/DegreeRepo.java
+++ src/main/java/ufp/esof/project/repositories/DegreeRepo.java
@@ -4,6 +4,9 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import ufp.esof.project.models.Degree;
 
+import java.util.Optional;
+
 @Repository
 public interface DegreeRepo extends CrudRepository<Degree, Long> {
+    Optional<Object> findByName(String name);
 }
